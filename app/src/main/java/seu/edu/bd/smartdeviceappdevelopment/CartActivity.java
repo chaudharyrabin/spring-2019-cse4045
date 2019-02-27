2dc9095a2f8ecdddd31edb3c329b4e74daef5586
Mahbub Hasan <mahbubhasan@Mahbubs-MacBook-Pro.local>
2019-02-27 16:09:16 +0600

    create data send and receive from 2 activities

--- app/src/main/java/seu/edu/bd/smartdeviceappdevelopment/CartActivity.java
+++ app/src/main/java/seu/edu/bd/smartdeviceappdevelopment/CartActivity.java
@@ -5,10 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class CartActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button backButton;
+    TextView downloadedData;
+    Bundle bundle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,15 +22,28 @@ public class CartActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onStart() {
         super.onStart();
+
         backButton = findViewById(R.id.back_button);
+        downloadedData = findViewById(R.id.downloaded_data);
+
+        bundle = getIntent().getExtras();
+        if(bundle != null){
+            String username = bundle.getString("user_name");
+            String password = bundle.getString("password");
+            String mobileNo = bundle.getString("mobile_no");
+
+
+            downloadedData.setText("UserName is : "+ username + "\nPassword is: "+password+"\n"+"Mobile No is: "+mobileNo);
+        }
+
 
         backButton.setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View v) {
         // back to main activity
-        Intent intent = new Intent(CartActivity.this, MainActivity.class);
-        startActivity(intent);
+        onBackPressed();
     }
 }
--- app/src/main/java/seu/edu/bd/smartdeviceappdevelopment/MainActivity.java
+++ app/src/main/java/seu/edu/bd/smartdeviceappdevelopment/MainActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -16,6 +17,7 @@ import seu.edu.bd.smartdeviceappdevelopment.fragment.ProductListFragment;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button clickMe;
+    EditText username, password, mobileno;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +33,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onStart();
         Log.d("ABC", "on start callback");
         clickMe = findViewById(R.id.next_page_button);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        mobileno = findViewById(R.id.mobileNo);
 
         // button on click
         clickMe.setOnClickListener(this);
@@ -47,7 +52,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         // call to another activity
+
+        String username = this.username.getText().toString();
+        String password = this.password.getText().toString();
+        String mobileNo = this.mobileno.getText().toString();
+
+
         Intent intent = new Intent(MainActivity.this, CartActivity.class);
+
+        intent.putExtra("user_name", username);
+        intent.putExtra("password",password);
+        intent.putExtra("mobile_no", mobileNo);
+
+
+
         startActivity(intent);
     }
 }
